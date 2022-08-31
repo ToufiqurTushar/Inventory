7d7d3052e6cc0b8bd5039b44cfb81595c3283e61
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2022-08-31 12:09:18 +0600

    Add icon

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -7,22 +7,22 @@
             </a>
             <div class="nav_list">
                 @can('view-any', App\Models\Customer::class)
-                    <a class="nav_link {{ (request()->segment(1) == 'customers') ? 'active' : '' }}" href="{{ route('customers.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Customers</span></a>
+                    <a class="nav_link {{ (request()->segment(1) == 'customers') ? 'active' : '' }}" href="{{ route('customers.index') }}"><i class="fa fa-person-chalkboard nav_logo-icon"></i><span class="nav_name">Customers</span></a>
                 @endcan
                 @can('view-any', App\Models\FoodEntry::class)
-                    <a class="nav_link {{ (request()->segment(1) == 'food-entries') ? 'active' : '' }}" href="{{ route('food-entries.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Food Entries</span></a>
+                    <a class="nav_link {{ (request()->segment(1) == 'food-entries') ? 'active' : '' }}" href="{{ route('food-entries.index') }}"><i class="fa fa-utensils nav_logo-icon"></i><span class="nav_name">Food Entries</span></a>
                 @endcan
                 @can('view-any', App\Models\FoodOrder::class)
-                    <a class="nav_link {{ (request()->segment(1) == 'food-orders') ? 'active' : '' }}" href="{{ route('food-orders.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Food Orders</span></a>
+                    <a class="nav_link {{ (request()->segment(1) == 'food-orders') ? 'active' : '' }}" href="{{ route('food-orders.index') }}"><i class="fa fa-cart-plus nav_logo-icon"></i><span class="nav_name">Food Orders</span></a>
                 @endcan
                 @can('view-any', App\Models\Member::class)
-                    <a class="nav_link {{ (request()->segment(1) == 'members') ? 'active' : '' }}" href="{{ route('members.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Members</span></a>
+                    <a class="nav_link {{ (request()->segment(1) == 'members') ? 'active' : '' }}" href="{{ route('members.index') }}"><i class="fa fa-people-group nav_logo-icon"></i><span class="nav_name">Members</span></a>
                 @endcan
                 @can('view-any', App\Models\MemberType::class)
                     <a class="nav_link {{ (request()->segment(1) == 'member-types') ? 'active' : '' }}" href="{{ route('member-types.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Member Types</span></a>
                 @endcan
                 @can('view-any', App\Models\StockIn::class)
-                    <a class="nav_link {{ (request()->segment(1) == 'stocks-in') ? 'active' : '' }}" href="{{ route('stocks-in.index') }}"><i class="fa fa-arrow-right nav_logo-icon"></i><span class="nav_name">Stocks In</span></a>
+                    <a class="nav_link {{ (request()->segment(1) == 'stocks-in') ? 'active' : '' }}" href="{{ route('stocks-in.index') }}"><i class="fa fa-layer-group nav_logo-icon"></i><span class="nav_name">Stocks In</span></a>
                 @endcan
             </div>
 
